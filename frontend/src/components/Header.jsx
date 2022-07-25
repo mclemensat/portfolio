73d73fb042ec60f31628f40b1afede9149e76b1c
Marie Clémensat <marieclemensat@wanadoo.fr>
2022-07-25 11:23:00 +0200

    navbar style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <div className="mb-16">
       <div className="bg-tertiary rounded-lg -mt-2">
-        <div className="py-24 text-[#1d3c45] text-center">
+        <div className="py-24 md:py-40 text-[#1d3c45] text-center">
           <div className="mb-10 mx-auto">
-            <h1 className="mb-6 font-sans text-6xl font-bold leading-none tracking-tight mx-auto">
+            <h1 className="mb-6 font-sans text-5xl md:text-7xl font-bold leading-none tracking-tight mx-auto">
               <span className="relative inline-block">
                 <svg
                   viewBox="0 0 52 24"
@@ -35,13 +35,15 @@ export default function Header() {
               <br></br>
               <span className="ml-12">Clémensat</span>
             </h1>
-            <p className="text-3xl text-center">Développeuse web</p>
+            <p className="sm:text-2xl md:text-3xl text-center">
+              Développeuse web
+            </p>
           </div>
         </div>
       </div>
-      <div className="relative px-4 sm:px-0 -mt-16">
-        <div className="absolute inset-0 h-1/2"/>
-        <div className="relative grid mx-auto overflow-hidden bg-slate-50 divide-y divide-[#1d3c45] border border-[#1d3c45] rounded shadow sm:divide-y-0 sm:divide-x sm:max-w-screen-sm sm:grid-cols-3 lg:max-w-screen-md">
+      <div className="relative px-4 sm:px-0 -mt-24">
+        <div className="absolute inset-0 h-1/2" />
+        <div className="relative grid mx-auto overflow-hidden bg-slate-50 divide-y  border border-4 border-[#D48166] rounded-lg shadow sm:divide-y-0 sm:max-w-screen-sm sm:grid-cols-3 lg:max-w-screen-md">
           <Link
             className="cursor-pointer text-center bg-primary"
             to="about"
@@ -49,8 +51,8 @@ export default function Header() {
             smooth={true}
           >
             <div className="inline-block p-8 hover:scale-105 duration-200">
-              <i className="my-2 fa-solid fa-circle-info text-3xl text-secondary"></i>
-              <p className="mx-4 text-lg text-secondary">A propos</p>
+              <i className="my-4 fa-solid fa-circle-info sm:text-3xl md:text-4xl text-secondary"></i>
+              <p className="mx-4 text-xl text-secondary">A propos</p>
             </div>
           </Link>
           <Link
@@ -60,8 +62,8 @@ export default function Header() {
             smooth={true}
           >
             <div className="inline-block p-8 hover:scale-105 duration-200">
-              <i className="my-2 fa-solid fa-code text-3xl text-secondary"></i>
-              <p className="mx-4 text-lg text-secondary">Portfolio</p>
+              <i className="my-4 fa-solid fa-code sm:text-3xl md:text-4xl text-secondary"></i>
+              <p className="mx-4 text-xl text-secondary">Portfolio</p>
             </div>
           </Link>
           <Link
@@ -71,8 +73,8 @@ export default function Header() {
             smooth={true}
           >
             <div className="inline-block p-8 hover:scale-105 duration-200">
-              <i className="my-2 fa-solid fa-address-card text-3xl text-secondary"></i>
-              <p className="mx-4 text-lg text-secondary">Contact</p>
+              <i className="my-4 fa-solid fa-address-card sm:text-3xl md:text-4xl text-secondary"></i>
+              <p className="mx-4 text-xl text-secondary">Contact</p>
             </div>
           </Link>
         </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,17 +8,21 @@ import Footer from "../components/Footer";
 export default function Home() {
   return (
     <>
-      <div className="mx-0 sm:mx-12 sm:mb-20">
+      <div className="mx-0 sm:mx-12 sm:mb-28">
         <Header />
       </div>
-      <div className="mx-4 sm:mb-20">
-        <h2 id="about">Présentation</h2>
-        <hr className="-mt-2 mb-12 mx-[200px] sm:mx-[650px] bg-[#FFF1E1] border border-[#FFF1E1]"></hr>
+      <div className="mx-4 sm:mb-20 md:mb-28">
+        <h2 className="text-4xl md:text-5xl" id="about">
+          Présentation
+        </h2>
+        <hr className="-mt-2 sm:mb-4 md:mb-20 mx-[200px] sm:mx-[650px] bg-[#FFF1E1] border border-[#FFF1E1]"></hr>
         <About />
       </div>
-      <div className="mx-4 sm:mb-20">
-        <h2 id="portfolio">Portfolio</h2>
-        <hr className="-mt-2 mb-12 mx-[200px] sm:mx-[650px] bg-[#FFF1E1] border border-[#FFF1E1]"></hr>
+      <div className="mx-4 md:mb-24">
+        <h2 className="text-4xl md:text-5xl" id="portfolio">
+          Portfolio
+        </h2>
+        <hr className="-mt-2 sm:mb-4 md:mb-20 mx-[200px] sm:mx-[650px] bg-[#FFF1E1] border border-[#FFF1E1]"></hr>
         <Portfolio />
       </div>
       {/* <div className="mx-4">
@@ -26,9 +30,11 @@ export default function Home() {
         <hr className="-mt-2 mb-12 mx-[200px] sm:mx-[650px] bg-black border border-black"></hr>
         <Skills />
       </div> */}
-      <div className="mx-4 sm:mb-20">
-        <h2 id="contact">Contact</h2>
-        <hr className="-mt-2 mb-12 mx-[200px] sm:mx-[650px] bg-[#FFF1E1] border border-[#FFF1E1]"></hr>
+      <div className="mx-4 sm:mb-24">
+        <h2 className="text-4xl md:text-5xl" id="contact">
+          Contact
+        </h2>
+        <hr className="-mt-2 sm:mb-4 md:mb-20 mx-[200px] sm:mx-[650px] bg-[#FFF1E1] border border-[#FFF1E1]"></hr>
         <Contact />
       </div>
       <div className="mt-20">
